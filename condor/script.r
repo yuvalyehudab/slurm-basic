14a06715cbdfa933179187bfcc58fa780abe2980
Yuval Barak <yuvalyehudab@mail.tau.ac.il>
2021-04-27 15:22:44 +0300

    make everything run

--- condor/script.r
+++ condor/script.r
@@ -3,5 +3,12 @@
 # author: yuvalyehudab
 
 print('hello world')
+.libPaths( c("/usr/lib/R/library", .libPaths()) )
+
+options(repos=structure(c(CRAN='http://cran.r-project.org')))
+install.packages('tidyr')
+
+library('tidyr')
+# after installation for Rscript
 print(Sys.getenv('PATH'))
-print('success!)
+print('success!')
---
+++ condor/test-udocker.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# name: test-udocker.sh
+# author: yuvalyehudab@mail.tau.ac.il
+
+echo 'hello world!'
+
+apt update && echo 'Done!'
